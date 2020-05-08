e9c191cdd52aac756a4842dd750e53608bffd0da
公司电脑 <18230794620@163.com>
2020-05-08 17:48:01 +0800

    都是

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,29 +1,214 @@
 <template>
-    <view>
-		<uni-card title="今天是个好天气" extra="2020/5/1" >
-		    <view class="show-content">
-				{{content}}
+    <view >
+		<!-- <uni-fab
+		:popMenu = "popMenu"
+		:pattern = "pattern"
+		></uni-fab> -->
+		<view class="info-list-area">
+			<view class="info-list-item" v-for="vlue in person">				
+				<view>
+					<!-- <view class="iconfont iconxiaocao"></view> -->
+					<view class="info-head-img-area">
+						<image class="info-head-img"
+						 src="../../static/imges/personal_center/cap-2923682_1920.jpg"
+						 mode = "aspectFill"
+						></image>
+					</view>
+					
+					<view class="qianming">						
+						<view class="info-name">{{vlue.name}}</view>
+						<view class="info-qianming">
+							<view class="iconfont iconpinglun"></view>
+							{{vlue.time}}
+							<!-- 须臾片刻也好，你是否会想起我呢？ -->
+						</view>
+					</view>
+
+				</view>
+				<view>						
+					<text class="infor-content">{{vlue.qianming}}</text>
+					<!-- <view class="time-text">
+						2017年6月13日
+					</view> -->
+				</view>				
+				<view class="info-bottom">
+					<view>
+						
+						<view class="address-text">
+							<view class="iconfont iconaddress"></view>
+							{{vlue.address}}
+						</view>						
+					</view>		 
+					<view class="flex-row">
+						<view class="iconfont iconzan1"></view>
+						<view class="iconfont iconpinglun1"></view>
+						<view class="iconfont iconzhuanfa"></view>
+					</view>			
+				</view>
 			</view>
-		</uni-card>
+
+		</view>
     </view>
+
+	
 </template>
 <script>
-	import uniCard from '@/components/uni-card/uni-card.vue'
+	import uniFab from "../../components/uni-fab/uni-fab.vue"
     export default {
         data() {
             return {
-               content:"我叫啊"
+				popMenu:false,
+				pattern:{
+					color:"#000",
+					buttonColor:"rgb(0,122,255)",
+					selectedColor:"#000"
+				},
+				person:[
+					{
+						name:"常开国",
+						time:"2020年5月20日",
+						address:"贵州省毕节市",
+						qianming:"早知如此，当初不要相遇就好了。",
+					},
+					{
+						name:"常开国",
+						time:"2020年5月20日",
+						address:"贵州省贵阳市",
+						qianming:"人类的伤痛并不相通，有时还会觉得有些吵闹。",
+					},
+					{
+						name:"常开国",
+						time:"2020年5月20日",
+						address:"贵州省贵阳市",
+						qianming:"这感情可能飞不过万水千山了..."
+					},
+					{
+						name:"常开国",
+						time:"2020年5月20日",
+						address:"贵州省贵阳市",
+						qianming:"当我们遇见某个人时，就不会感觉到孤单了。"
+					},
+					{
+						name:"常开国",
+						time:"2020年5月20日",
+						address:"贵州省贵阳市",
+						qianming:"曾经我也以为我是那个最特别的人。 可惜我不是。我自以为擅长的东西，总有人比我做得更好。我自以为与众不同的地方，其实也没什么不一样。 相比之下，我不过是一个很普通的人。 可能我拼命努力下也曾有一两次比一些人做得更好，但置身于人海之中，我顶多算是一颗大一点的鹅卵石。我很努力，但好像缺少点天赋和运气。有很多梦想，但实现起来也遥遥无期。但我没有放弃。我慢慢也能接受我是一个普通人的事实，但我也希望自己能追求喜欢的事物，哪怕回报我的不多。 也许这世界上真的存在着那样的人。存在着我永远都追不上的人，存在着我穷尽一生都难以望其项背的人。但即使再普通，我也希望活出一点点不一样的光芒。 我祈求有好运降临，我也渴望被命运所爱。但如果没有，我也不会放弃。"
+					}
+				]
             }
         },
         methods: {
 
         },
-		components :{uniCard}
+		components :{uniFab}
     }
 </script>
 
 <style>
-	.show-content{
+	@import url("https://at.alicdn.com/t/font_1787456_etzftms37n7.css");
+	.address-text,.iconpinglun,.iconaddress{
+		padding-right: 8upx;
+	}
+	.iconzan1,.iconpinglun1,.iconzhuanfa{
+		padding: 0 8px;		
+		font-size:25px;
+		top: -9px;
+	}
+	.iconzan1{
+		top: -13px;
+	}
+	.iconpinglun1{
+		font-size: 25px !important;
+	}
+	.time-text,.iconxiaocao,.info-name,.qianming,.iconaddress,.iconzan1,.iconpinglun1,.iconzhuanfa{
+		position: relative;
+	}
+	.info-name{
+		margin-left: 3px;
+		margin-top: 15upx;
+	}
+	.address-text,.time-text{
+		font-size:15px;
+	}
+	.infor-content{
+		padding: 20upx 20upx 0upx 10upx;
+	}
+	.iconaddress{
+		top: 3px;
+	}
+	.iconpinglun,.time-text,.address-text{
+		color:#c2c2c2;
+	}
+	.info-list-area,.info-bottom,.info-list-item,.info-list-item :nth-child(1),.qianming,.info-qianming,.flex-row{
+		display: flex;		
+	}
+	.qianming,.info-list-area{
+		justify-content: center;
+	}
+	.flex-row,.info-list-area{
+		align-items: center;
+		align-items: center;
+	}
+	.info-list-area{
+		background-color: #eee;		
+		flex-direction: column;
+		padding-left: 15upx;
+	}
+	.flex-row{
+		flex-direction: row;
+	}
+	.time-text-area{
+
+	}
+	.info-bottom{
+		flex-direction: row;
+		justify-content: space-between;
+		margin-top: 10px;
+	}
+	.info-list-item{
+		width: 100%;
+		margin: 40upx 0;
+		margin-top: 0;
+		background-color: #fff;
+		flex-direction: column;
+		padding: 10px 30px 0px 30px;
+		
+	}
+	.info-list-area .info-list-item :nth-child(1){
+		flex-direction: row;
+	}
+	.info-list-area .info-list-item :nth-child(2){
 		font-size: 15px;
+		letter-spacing: 0.5px;
+		margin:8upx;
+	}
+	.info-head-img-area{
+		width: 110upx;
+		height: 110upx;
+		padding:5upx;
+	}
+	.info-head-img-area image{
+		height: 100%;
+		width: 100%;
+		border-radius: 50%;
+		border: 3px solid #fff;	
+	}
+	.qianming{
+		flex-direction: column;
+		align-items:flex-start;
+	}
+	.info-list-area .info-list-item .info-qianming{
+		font-size: 12px;
+	}
+	
+	.info-list-area .info-list-item .info-name{
+		color: #3f3838;
+		font-size: 15px;
+	}
+	.shadow{
+		height: 100%;
+		width: 100%;
+		position: absolute;
+		background-color: rgba(0,0,0,.5);
 	}
 </style>
